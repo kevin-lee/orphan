f069ab3aac18f01d57b5232838b7c37d045dad33
Kevin Lee <kevin.code@kevinlee.io>
2025-08-02 21:40:42 +1000

    Close #14: [orphan-cats] Add CatsSemigroup for cats.kernel.Semigroup
    - Add `OrphanCatsKernel` trait with `CatsSemigroup` type alias and `@implicitNotFound` annotation
    - Implement `CatsSemigroup` instances for both Scala 2 and Scala 3 - Not actual instances but to make an instance of `cats.kernel.Semigroup` type-class check if `cats` is available.
    - Add `OrphanCatsKernelInstances` with `MySemigroup` `trait` and `MyNum` case class for testing
    - Add comprehensive test coverage in `CatsSemigroupWithoutCatsSpec`
    - Add expected error message for missing `cats.kernel.Semigroup` dependency to ensure proper compile-time error when cats library is not available

---
+++ modules/orphan-cats-test-without-cats/shared/src/test/scala-3/orphan_test/CatsSemigroupWithoutCatsSpec.scala
@@ -0,0 +1,45 @@
+package orphan_test
+
+import hedgehog.*
+import hedgehog.runner.*
+import orphan_instance.OrphanCatsKernelInstances.{MyNum, MySemigroup}
+
+/** @author Kevin Lee
+  * @since 2025-07-28
+  */
+object CatsSemigroupWithoutCatsSpec extends Properties {
+
+  override def tests: List[Test] = List(
+    property("test MySemigroup.combine", testMySemigroupMap),
+    example("test CatsSemigroup.combine", testCatsSemigroup),
+  )
+
+  def testMySemigroupMap: Property = for {
+    n1     <- Gen.int(Range.linear(0, Int.MaxValue)).log("n1")
+    n2     <- Gen.int(Range.linear(0, Int.MaxValue)).log("n2")
+    myNum1 <- Gen.constant(MyNum(n1)).log("myNum1")
+    myNum2 <- Gen.constant(MyNum(n2)).log("myNum2")
+  } yield {
+    val expected = MyNum(n1 + n2)
+    val actual   = MySemigroup[MyNum].combine(myNum1, myNum2)
+    actual ==== expected
+  }
+
+  def testCatsSemigroup: Result = {
+
+    import scala.compiletime.testing.typeCheckErrors
+    val expectedMessage = orphan_test.ExpectedMessages.ExpectedMessageForCatsSemigroup
+
+    val actual = typeCheckErrors(
+      """
+        val _ = orphan_instance.OrphanCatsKernelInstances.MyNum.catsSemigroup
+      """
+    )
+
+    val actualErrorMessage = actual.map(_.message).mkString
+    Result
+      .assert(actualErrorMessage.startsWith(expectedMessage))
+      .log("The actual error message doesn't start with the expected one.")
+  }
+
+}
--- modules/orphan-cats-test-without-cats/shared/src/test/scala/orphan_test/ExpectedMessages.scala
+++ modules/orphan-cats-test-without-cats/shared/src/test/scala/orphan_test/ExpectedMessages.scala
@@ -13,4 +13,7 @@ object ExpectedMessages {
   val ExpectedMessageForCatsMonad: String =
     """Missing an instance of `CatsMonad` which means you're trying to use cats.Monad, but cats library is missing in your project config. If you want to have an instance of cats.Monad[F[*]] provided, please add `"org.typelevel" %% "cats-core" % CATS_VERSION` to your libraryDependencies in build.sbt"""
 
+  val ExpectedMessageForCatsSemigroup: String =
+    """Missing an instance of `CatsSemigroup` which means you're trying to use cats.kernel.Semigroup, but cats library is missing in your project config. If you want to have an instance of cats.kernel.Semigroup[F[*]] provided, please add `"org.typelevel" %% "cats-core" % CATS_VERSION` to your libraryDependencies in build.sbt"""
+
 }

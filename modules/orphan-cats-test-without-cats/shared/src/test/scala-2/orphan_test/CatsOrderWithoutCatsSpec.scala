ff3e434b62fd8bbe8ba8d5a1daaf76d41ae8ec80
Kevin Lee <kevin.code@kevinlee.io>
2025-08-03 21:42:26 +1000

    Close #26: [orphan-cats] Add CatsOrder for cats.kernel.Order
    - Add `CatsOrder` type alias to `OrphanCatsKernel`
    - Implement `CatsOrder` `trait` with `@implicitNotFound` annotation with helpful error messages for missing cats-core dependency.
    - Add `MyOrder` `trait` and instances to test for both Scala 2 and 3
    - Add `MyOrder` `trait` and `instance`s in test code.
    - Add `catsOrder` instances (`implicit`/`given`) for testing.

---
+++ modules/orphan-cats-test-without-cats/shared/src/test/scala-2/orphan_test/CatsOrderWithoutCatsSpec.scala
@@ -0,0 +1,40 @@
+package orphan_test
+
+import hedgehog.*
+import hedgehog.runner.*
+import orphan.testing.CompileTimeError
+import orphan_instance.OrphanCatsKernelInstances.{MyNum, MyOrder}
+
+/** @author Kevin Lee
+  * @since 2025-07-28
+  */
+object CatsOrderWithoutCatsSpec extends Properties {
+
+  override def tests: List[Test] = List(
+    property("test MyOrder.compare(A, A)", testMyOrderCompare),
+    example("test cats.Order", testCatsOrder),
+  )
+
+  def testMyOrderCompare: Property = for {
+    n1     <- Gen.int(Range.linear(0, Int.MaxValue)).log("n1")
+    n2     <- Gen.int(Range.linear(0, Int.MaxValue)).log("n2")
+    myNum1 <- Gen.constant(MyNum(n1)).log("myNum1")
+    myNum2 <- Gen.constant(MyNum(n2)).log("myNum2")
+  } yield {
+    val expected = n1.compare(n2)
+    val actual   = MyOrder[MyNum].compare(myNum1, myNum2)
+    actual ==== expected
+  }
+
+  def testCatsOrder: Result = {
+    val expected = s"""error: ${ExpectedMessages.ExpectedMessageForCatsOrder}
+                      |orphan_instance.OrphanCatsKernelInstances.MyNum.catsOrder
+                      |                                                ^""".stripMargin
+
+    val actual = CompileTimeError.from(
+      "orphan_instance.OrphanCatsKernelInstances.MyNum.catsOrder"
+    )
+    actual ==== expected
+  }
+
+}

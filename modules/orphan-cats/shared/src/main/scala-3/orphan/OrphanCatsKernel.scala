b6ffd4c791724c9b2eaa8b08959379f0a5b638c8
Kevin Lee <kevin.code@kevinlee.io>
2025-08-03 19:30:56 +1000

    Fix implicitNotFound messages for CatsSemigroup and CatsMonoid

--- modules/orphan-cats/shared/src/main/scala-3/orphan/OrphanCatsKernel.scala
+++ modules/orphan-cats/shared/src/main/scala-3/orphan/OrphanCatsKernel.scala
@@ -14,7 +14,7 @@ private[orphan] object OrphanCatsKernel {
   @implicitNotFound(
     msg = "Missing an instance of `CatsSemigroup` which means you're trying to use cats.kernel.Semigroup, " +
       "but cats library is missing in your project config. " +
-      "If you want to have an instance of cats.kernel.Semigroup[F[*]] provided, " +
+      "If you want to have an instance of cats.kernel.Semigroup[A] provided, " +
       """please add `"org.typelevel" %% "cats-core" % CATS_VERSION` to your libraryDependencies in build.sbt"""
   )
   sealed protected trait CatsSemigroup[F[*]]
@@ -27,7 +27,7 @@ private[orphan] object OrphanCatsKernel {
   @implicitNotFound(
     msg = "Missing an instance of `CatsMonoid` which means you're trying to use cats.kernel.Monoid, " +
       "but cats library is missing in your project config. " +
-      "If you want to have an instance of cats.kernel.Monoid[F[*]] provided, " +
+      "If you want to have an instance of cats.kernel.Monoid[A] provided, " +
       """please add `"org.typelevel" %% "cats-core" % CATS_VERSION` to your libraryDependencies in build.sbt"""
   )
   sealed protected trait CatsMonoid[F[*]]

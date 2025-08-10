6ebf601bc70633e670ba89b40e0d4388e9973fca
Kevin Lee <kevin.code@kevinlee.io>
2025-08-11 09:28:32 +1000

    Close #41: [`orphan-circe`] Add `OrphanCirce` with `CirceEncoder` for circe support
    Add phantom CirceEncoder gate and tests (Scala 2/3)
    
    Introduce `OrphanCirce` (Scala 2 & 3) with a phantom `CirceEncoder[F[*]]` and
    `@implicitNotFound` guidance to require circe-core when an `io.circe.Encoder` is used.
    
    - OrphanCirce (Scala 2/3):
      - `orphan.OrphanCirce` defines `CirceEncoder[F[*]]` (phantom).
      - Provides an implicit/given for `io.circe.Encoder` to gate usage.
      - Clear `@implicitNotFound` message instructs adding `"io.circe" %% "circe-core" % CIRCE_VERSION`.
    
    - Instances/examples:
      - `orphan_instance.OrphanCirceInstances` with `MyBox` and a derived `io.circe.Encoder`.
      - Gated by `CirceEncoder[F[*]]` (implicit in Scala 2, given in Scala 3).
    
    - Tests:
      - With circe: `CirceEncoderWithCirceSpec` asserts `MyBox` JSON matches expected.
      - Without circe:
        - Scala 2: checks compile-time error via `extras.testing.CompileTimeError`.
        - Scala 3: checks with `scala.compiletime.testing.typeCheckErrors`.
      - Shared `ExpectedMessages` centralizes the expected error message.
      - Check compile-time error in both Scala 2 and 3.
    
    - Build:
      - Update build.sbt to wire new modules and test dependencies.

---
+++ modules/orphan-circe-test/shared/src/main/scala-3/orphan_instance/OrphanCirceInstances.scala
@@ -0,0 +1,24 @@
+package orphan_instance
+
+import scala.annotation.nowarn
+import orphan.OrphanCirce
+
+/** @author Kevin Lee
+  * @since 2025-08-10
+  */
+object OrphanCirceInstances {
+
+  final case class MyBox(id: Int, name: String, isActive: Boolean)
+  object MyBox extends MyCirceInstances
+
+  private[orphan_instance] trait MyCirceInstances extends OrphanCirce {
+    @nowarn(
+      """msg=evidence parameter .+ of type (.+\.)*CirceEncoder\[F\] in method circeEncoder is never used"""
+    )
+    @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+    given circeEncoder[F[*]: CirceEncoder]: F[MyBox] = {
+      io.circe.generic.semiauto.deriveEncoder[MyBox].asInstanceOf[F[MyBox]] // scalafix:ok DisableSyntax.asInstanceOf
+    }
+  }
+
+}

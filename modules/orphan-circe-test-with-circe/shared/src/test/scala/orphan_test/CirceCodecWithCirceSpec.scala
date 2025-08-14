d9db680ac00ce91471d5f4f954695db5d51c44ed
Kevin Lee <kevin.code@kevinlee.io>
2025-08-14 21:36:31 +1000

    Issue #43: [`orphan-circe`] Add `CirceCodec` for circe to `OrphanCirce` - Fix and add more tests

--- modules/orphan-circe-test-with-circe/shared/src/test/scala/orphan_test/CirceCodecWithCirceSpec.scala
+++ modules/orphan-circe-test-with-circe/shared/src/test/scala/orphan_test/CirceCodecWithCirceSpec.scala
@@ -13,7 +13,9 @@ import orphan_instance.OrphanCirceInstances.MyBoxForCodec
 object CirceCodecWithCirceSpec extends Properties {
 
   override def tests: List[Test] = List(
-    property("testCirceCodec", testCirceCodec)
+    property("round-trip test CirceCodec", testCirceCodec),
+    property("test CirceCodec encoding", testCirceCodecEncoding),
+    property("test CirceCodec decoding", testCirceCodecDecoding),
   )
 
   def testCirceCodec: Property = for {
@@ -33,10 +35,7 @@ object CirceCodecWithCirceSpec extends Properties {
     val encodingResult = actualJson ==== expectedJson
 
     val expected       = myBox
-    val decodingResult = decode[MyBoxForCodec](actualJson.noSpaces).matchPattern {
-      case Right(actual) =>
-        actual ==== expected
-    }
+    val decodingResult = decode[MyBoxForCodec](actualJson.noSpaces) ==== Right(expected)
 
     Result.all(
       List(
@@ -44,6 +43,42 @@ object CirceCodecWithCirceSpec extends Properties {
         decodingResult,
       )
     )
+
+  }
+
+  def testCirceCodecEncoding: Property = for {
+    id       <- Gen.int(Range.linear(0, Int.MaxValue)).log("id")
+    name     <- Gen.string(Gen.alpha, Range.linear(0, 10)).log("name")
+    isActive <- Gen.boolean.log("isActive")
+    myBox    <- Gen.constant(MyBoxForCodec(id, name, isActive)).log("myBox")
+  } yield {
+    val expected =
+      json"""{
+               "id":$id,
+               "name":$name,
+               "isActive":$isActive
+             }"""
+
+    val actual = myBox.asJson
+    actual ==== expected
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.Equals"))
+  def testCirceCodecDecoding: Property = for {
+    id            <- Gen.int(Range.linear(0, Int.MaxValue)).log("id")
+    name          <- Gen.string(Gen.alpha, Range.linear(0, 10)).log("name")
+    isActive      <- Gen.boolean.log("isActive")
+    myBoxForCodec <- Gen.constant(MyBoxForCodec(id, name, isActive)).log("myBoxForCodec")
+  } yield {
+    val jsonInput =
+      json"""{
+               "id":$id,
+               "name":$name,
+               "isActive":$isActive
+             }"""
+
+    val expected = myBoxForCodec
+    decode[MyBoxForCodec](jsonInput.noSpaces) ==== Right(expected)
 
   }
 

ed66d81e04b5dcdc421e0950ffd1b10d7fb73a1b
Kevin Lee <kevin.code@kevinlee.io>
2025-08-22 00:08:48 +1000

    Close #54: [orphan-cats] Add CatsInvariant for cats.Invariant
    - Add `CatsInvariant` to `OrphanCats`
    - Add `MyInvariant` trait and companion object for both Scala 2 and 3 for testing
    - Implement `MyInvariant` instance for `MyBox` type for testing

---
+++ modules/orphan-cats-test-without-cats/shared/src/test/scala-3/orphan_test/CatsInvariantWithoutCatsSpec.scala
@@ -0,0 +1,40 @@
+package orphan_test
+
+import hedgehog.*
+import hedgehog.runner.*
+import orphan_instance.OrphanCatsInstances.{MyBox, MyInvariant}
+
+/** @author Kevin Lee
+  * @since 2025-08-21
+  */
+object CatsInvariantWithoutCatsSpec extends Properties {
+
+  override def tests: List[Test] = List(
+    property("test MyInvariant.imap", testMyInvariantMap),
+    example("test CatsInvariant", testCatsInvariant),
+  )
+
+  def testMyInvariantMap: Property = for {
+    n     <- Gen.int(Range.linear(0, Int.MaxValue)).log("n")
+    myBox <- Gen.constant(MyBox(n)).log("myBox")
+  } yield {
+    val expected = MyBox((n + 999).toString)
+    val actual   = MyInvariant[MyBox].imap(myBox)(a => (a + 999).toString)(_.toInt - 999)
+    actual ==== expected
+  }
+
+  def testCatsInvariant: Result = {
+    import scala.compiletime.testing.typeCheckErrors
+    val expectedMessage = ExpectedMessages.ExpectedMessageForCatsInvariant
+
+    val actual = typeCheckErrors(
+      """
+        val _ =  orphan_instance.OrphanCatsInstances.MyBox.catsInvariant
+      """
+    )
+
+    val actualErrorMessage = actual.map(_.message).mkString
+    (actualErrorMessage ==== expectedMessage)
+  }
+
+}

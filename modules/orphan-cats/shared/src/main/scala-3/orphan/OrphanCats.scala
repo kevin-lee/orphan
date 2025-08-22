14e3efe6eb972f3696c8b9f1f2a866234e260d56
Kevin Lee <kevin.code@kevinlee.io>
2025-08-23 08:44:30 +1000

    Close #60: Update `implicitNotFound` message for `OrphanCats.CatsShow`

--- modules/orphan-cats/shared/src/main/scala-3/orphan/OrphanCats.scala
+++ modules/orphan-cats/shared/src/main/scala-3/orphan/OrphanCats.scala
@@ -21,7 +21,7 @@ private[orphan] object OrphanCats {
   @implicitNotFound(
     msg = "Missing an instance of `CatsShow` which means you're trying to use cats.Show, " +
       "but cats library is missing in your project config. " +
-      "If you want to have an instance of cats.Show[F[*]] provided, " +
+      "If you want to have an instance of cats.Show[A] provided, " +
       """please add `"org.typelevel" %% "cats-core" % CATS_VERSION` to your libraryDependencies in build.sbt"""
   )
   sealed protected trait CatsShow[F[*]]
